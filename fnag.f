d98f78153534abae47c286280de8d3e9fa848668
Ricardo Yanez <yanezr@oregonstate.edu>
2022-10-23 06:25:48 -0700

    Substitute to D02BBF with RKSUITE

--- fnag.f
+++ fnag.f
@@ -1,7 +1,7 @@
 C
 C     Copyright (c) 2022 Ricardo Yanez <ricardo.yanez@calel.org>
 C
-C     C wrapper to the NAG Fortran Library
+C     Wrappers to the NAG Fortran Library
 C
 C
 C     This program is free software; you can redistribute it and/or modify
@@ -22,100 +22,133 @@ C
 C     NAG X05BAF returns the amount of processor time used since an unspecified
 C     previous time, via the routine name.
 C
-C     Calls the C wrapper function _X05BAF(double *elaps)
+C     THe C function c_x05baf is used to substitute X05BAF by calling clock().
 C
-      real*8 function X05BAF()
-      real*8 elaps
-C      write(*,*) '*** call to X05BAF'
-      call _X05BAF(elaps)
-      X05BAF = elaps
-      return
-      end
+      REAL*8 FUNCTION X05BAF()
+      IMPLICIT REAL*8(A-H,O-Z)
+      X05BAF = C_X05BAF()
+c$$$      write(*,*) '*** call to X05BAF=',X05BAF
+      RETURN
+      END
 
-      subroutine C05ADF(A,B,EPS,ETA,F,X,FAIL)
-      write(*,*) '*** call to C05ADF'
+C     NAG C05ADF locates a zero of a continuous function in a given interval
+C     by a combination of the methods of linear interpolation, extrapolation
+C     and bisection.
+C
+C     The Numerical Recipes ZBRENT function is used to substitute C05ADF
+C
+      SUBROUTINE C05ADF(A,B,EPS,ETA,F,X,IFAIL)
+      IMPLICIT REAL*8(A-H,O-Z)
+      EXTERNAL F
+      X=ZBRENT(F,A,B,ETA,IFAIL)
+c$$$      write(*,*) '*** call to C05ADF',X,F(X)
+      RETURN
+      END
+
+C     D02BBF solves an initial value problem for a first-order system of
+C     ordinary differential equations using Runge-Kutta methods.
+C
+C     RKSUITE is used to substitute D02BBF
+C
+      subroutine D02BBF(X,XEND,N,Y,TOL,IRELAB,FCN,OUTPUT,W,IFAIL)
+      IMPLICIT REAL*8(A-H,O-Z)
+      EXTERNAL FCN
+      DIMENSION Y(N),YPGOT(N),YMAX(N)
+      DIMENSION THRES(N)
+      DIMENSION WORK(32*N)
+      INTEGER UFLAG
+      DO I=1,N
+        THRES(I)=TOL
+      ENDDO
+      CALL SETUP(N,X,Y,XEND,TOL,THRES,2,'U',.FALSE.,0.0D0,WORK,32*N,
+     +     .TRUE.)
+      CALL UT(FCN,XEND,TGOT,Y,YPGOT,YMAX,WORK,UFLAG)
+      IF (UFLAG.EQ.1) THEN
+        IFAIL=0
+      ELSE
+        WRITE(*,*)'D02BBF: Error in RKSUITE'
+        stop
+      ENDIF
+c$$$      write(*,*) '*** call to D02BBF'
       return
       end
 
       subroutine C05AVF(X,FX,H,BOUNDL,BOUNDU,Y,C,IND,IFAIL)
       write(*,*) '*** call to C05AVF'
+      stop
       return
       end
 
       subroutine E01BEF(N,X,F,D,IFAIL)
       write(*,*) '*** call to E01BEF'
+      stop
       return
       end
 
       subroutine E01BFF(N,X,F,D,M,PX,PF,IFAIL)
       write(*,*) '*** call to E01BFF'
+      stop
       return
       end
  
       subroutine E01BGF(N,X,F,D,M,PX,PF,PD,IFAIL)
       write(*,*) '*** call to E01BGF'
+      stop
       return
       end
 
       subroutine D01ASF(G,A,OMEGA,KEY,EPSABS,RESULT,ABSERR,
      &     LIMLST,LST,ERLST,RSLST,IERLST,W,LW,IW,LIW,IFAIL)
       write(*,*) '*** call to D01ASF'
+      stop
       return
       end
 
       subroutine D01AMF(F,BOUND,INF,EPSABS,EPSREL,RESULT,ABSERR,
      &     W,LW,IW,LIW,IFAIL)
       write(*,*) '*** call to D01AMF'
+      stop
       return
       end
 
       subroutine D01GAF(X,Y,N,ANS,ER,IFAIL)
       write(*,*) '*** call to D01GAF'
+      stop
       return
       end
-
-C     D02PQF is a setup routine which must be called prior to the first call
-C     of either of the integration routines D02PEF and D02PFF.
-      subroutine D02PQF(N,TSTART,TEND,YINIT,TOL,THRESH,METHOD,HSTART,
-     &     IWSAV,RWSAV,IFAIL)
-      write(*,*) '*** call to D02PQF'
-      return
-      end
-
-C     D02PEF solves an initial value problem for a first-order system of
-C     ordinary differential equations using Runge–Kutta methods.
-      subroutine D02PEF (F,N,TWANT,TGOT,YGOT,YPGOT,YMAX,IUSER,RUSER,
-     &     IWSAV,RWSAV,IFAIL)
-      write(*,*) '*** call to D02PEF'
-      return
-      end
-
 
       subroutine S18DEF(FNU,Z,N,SCALE,CY,NZ,IFAIL)
       write(*,*) '*** call to S18DEF'
+      stop
       return
       end
+
       real function S14AAF(X,IFAIL)
       write(*,*) '*** call to S14AaF'
+      stop
       return
       end
       
       real function S14ABF(X,IFAIL)
       write(*,*) '*** call to S14ABF'
+      stop
       return
       end
 
       real function S15ADF(X,IFAIL)
       write(*,*) '*** call to S15ADF'
+      stop
       return
       end
 
       real function S18AEF(X,IFAIL)
       write(*,*) '*** call to S18AEF'
+      stop
       return
       end
 
       real function S18AFF(X, IFAIL)
       write(*,*) '*** call to S18AFF'
+      stop
       return
       end
