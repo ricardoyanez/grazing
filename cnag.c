b19743acc9369c806e7617795de4a5c6f1c48973
Ricardo Yanez <yanezr@oregonstate.edu>
2022-11-29 20:43:02 -0800

    C wrappers

--- cnag.c
+++ cnag.c
@@ -101,7 +101,7 @@ double c_s15adf_(double *x, int *ifail) {
 
 /*
  * This function is a C port of the FORTRAN subroutine D1MACH used by RKSUITE,
- * QUADPACK and Amos. The original FORTRAN version of D1MACH can be found in
+ * QUADPACK and AMOS. The original FORTRAN version of D1MACH can be found in
  * Netlib.
  *
  */
@@ -124,6 +124,3 @@ double c_d1mach_(int *i) {
   }
   return EXIT_SUCCESS;
 }
-
-
-
