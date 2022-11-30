7481e413a748e552114f2be68c0bbe61fc94c7d9
Ricardo Yanez <yanezr@oregonstate.edu>
2022-11-29 20:47:53 -0800

    nr updates

--- nrf77/zbrac.f
+++ nrf77/zbrac.f
@@ -1,4 +1,6 @@
       SUBROUTINE ZBRAC(FUNC,X1,X2,SUCCES)
+      IMPLICIT REAL*8(A-H,O-Z)
+      EXTERNAL FUNC
       PARAMETER (FACTOR=1.6,NTRY=50)
       LOGICAL SUCCES
       IF(X1.EQ.X2)PAUSE 'You have to guess an initial range'
@@ -6,8 +8,8 @@
       F2=FUNC(X2)
       SUCCES=.TRUE.
       DO 11 J=1,NTRY
-        IF(F1*F2.LT.0.)RETURN
-        IF(ABS(F1).LT.ABS(F2))THEN
+        IF(F1*F2.LT.0.D0)RETURN
+        IF(DABS(F1).LT.DABS(F2))THEN
           X1=X1+FACTOR*(X1-X2)
           F1=FUNC(X1)
         ELSE
--- nrf77/zbrak.f
+++ nrf77/zbrak.f
@@ -1,5 +1,7 @@
       SUBROUTINE ZBRAK(FX,X1,X2,N,XB1,XB2,NB)
-      DIMENSION XB1(1),XB2(1)
+      IMPLICIT REAL*8(A-H,O-Z)
+      EXTERNAL FX
+      DIMENSION XB1(NB),XB2(NB)
       NBB=NB
       NB=0
       X=X1
@@ -8,7 +10,7 @@
       DO 11 I=1,N
         X=X+DX
         FC=FX(X)
-        IF(FC*FP.LT.0.) THEN
+        IF(FC*FP.LT.0.D0) THEN
           NB=NB+1
           XB1(NB)=X-DX
           XB2(NB)=X
